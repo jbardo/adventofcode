4a47a96b2be29930fa9505403626c49729a358df
Julien Bardonnet <julien.bardonnet@gmail.com>
2021-12-06 10:32:33 +0100

    remove unused var

--- 2021/day6-exponential_growth.py
+++ 2021/day6-exponential_growth.py
@@ -35,7 +35,6 @@ def batch_grow(elements, nrep):
     return counts
 
 
-nrep = 80
 t1 = perf_counter()
 res1 = len(naive_grow(raw, 80))
 t2 = perf_counter()
@@ -43,5 +42,4 @@ res1bis = sum(batch_grow(raw, 80))
 t3 = perf_counter()
 print("#1:", res1, f"naive time elapsed: {t2 - t1:.9f} s")
 print("#1:", res1bis, f"batch time elapsed: {t3 - t2:.9f} s")
-nrep2 = 256
 print("#2:", sum(batch_grow(raw, 256)))
